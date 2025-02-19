9f097b3753177c1c438f3928f4220fe08b61181a
diptanshu1044 <diptanshubanerjee123@gmail.com>
2025-02-19 17:34:12 +0530

    .env added to gitignore

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ import { config } from "dotenv";
 config();
 
 const PORT = process.env.PORT || 5000;
+const CORS_ORIGIN = process.env.CORS_ORIGIN.split(", ");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: CORS_ORIGIN,
   },
 });
 
